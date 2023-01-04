8bb8b2229ea9c7518ecae333be0c1f1770e80688
qeaml <qeaml@protonmail.com>
2023-01-04 23:09:11 +0100

    Split CLI into smaller functions + add git

--- qcbs/__main__.py
+++ qcbs/__main__.py
@@ -1,146 +1,24 @@
 import qcbs.job as job
 import qcbs.c as c
+import qcbs.cli as cli
 from qcbs.common import *
 import sys
 import yaml
 import colorama
 from pathlib import Path
-from time import time
-from dataclasses import dataclass
-from typing import Any
-from datetime import datetime
-
-@dataclass
-class Args:
-  project: str
-  root: str
-  src: str
-  bin: str
-  obj: str
-
-  cc: str
-  exe: str
-  libs: list[str]
-  link: list[str]
-  incl: list[Path]
-
-  clean: bool
-  debug: bool
-  init: bool
-
-  @classmethod
-  def parse(cls, args: list[str]) -> "Args":
-    pos = []
-    named: dict[str, Any] = {}
-    for a in args:
-      if a.startswith("--"):
-        if '=' in a[2:]:
-          name, *value_elems = a[2:].split('=')
-          named[name] = '='.join(value_elems)
-        else:
-          named[a[2:]] = True
-      else:
-        pos.append(a)
-
-    project = "."
-    if len(pos) > 1:
-      project = pos[0]
-    if len(pos) > 2:
-      warn("Excess positional arguments ignored.")
-
-    parsed = Args.from_raw_dict(named)
-
-    parsed.clean = named.get("clean", False)
-    parsed.debug = named.get("debug", False)
-    parsed.init = named.get("init", False)
-
-    return parsed
-
-  @classmethod
-  def from_raw_dict(cls, data: dict[str, str]) -> "Args":
-    project = "."
-    root = data.get("root", project)
-    src = data.get("src", root)
-    bin = data.get("bin", root)
-    obj = data.get("obj", bin)
-    exe = data.get("exe", "")
-    cc = data.get("cc", "")
-    libs: list[str] = data.get("lib", "").split(",")
-    link: list[str] = data.get("link", "").split(",")
-    incl: list[str] = data.get("incl", "").split(",")
-
-    if libs == [""]:
-      libs = []
-    if link == [""]:
-      link = []
-    if incl == [""]:
-      incl = []
-
-    return cls(
-      project, root, src, bin, obj,
-      cc, exe, libs, link, [Path(p) for p in incl],
-      False, False, False
-    )
-
-  @classmethod
-  def from_parsed_dict(cls, data: dict[str, Any]) -> "Args":
-    project = "."
-    root = data.get("root", project)
-    src = data.get("src", root)
-    bin = data.get("bin", root)
-    obj = data.get("obj", bin)
-    exe = data.get("exe", "")
-    cc = data.get("cc", "")
-    libs: list[str] = data.get("lib", [])
-    link: list[str] = data.get("link", [])
-    incl: list[Path] = data.get("incl", [])
-
-    return cls(
-      project, root, src, bin, obj,
-      cc, exe, libs, link, incl,
-      False, False, False
-    )
-
-
-  @classmethod
-  def default(cls) -> "Args":
-    project = "."
-    return cls(
-      project, project, project, project, project,
-      "", "", [], [], [],
-      False, False, False
-    )
-
-  def combine(self, other: "Args") -> "Args":
-    project = other.project if other.project != self.project else self.project
-    root = other.root if other.root != self.root else self.root
-    src = other.src if other.src != self.src else self.src
-    bin = other.bin if other.bin != self.bin else self.bin
-    obj = other.obj if other.obj != self.obj else self.obj
-    exe = other.exe if other.exe != self.exe else self.exe
-    cc = other.cc if other.cc != self.cc else self.cc
-    libs = other.libs if other.libs != self.libs else self.libs
-    link = other.link if other.link != self.link else self.link
-    incl = other.incl if other.incl != self.incl else self.incl
-    return Args(
-      project, root, src, bin, obj,
-      cc, exe, libs, link, incl,
-      self.debug or other.debug, self.clean or other.clean, self.init or other.init
-    )
 
 def main() -> None:
   colorama.just_fix_windows_console()
-  start = time()
 
-  args_cmd = Args.parse(sys.argv)
+  args_cmd = cli.Args.parse(sys.argv)
   cmd_project_path = Path(args_cmd.project)
   build_file = cmd_project_path.joinpath("qcbs.yaml")
-  args_bf = Args.default()
+  args_bf = cli.Args.default()
   if build_file.exists():
     with build_file.open('rt') as f:
       build_info = yaml.load(f, Loader=yaml.Loader)
       if build_info is not None:
-        args_bf = Args.from_parsed_dict(build_info)
+        args_bf = cli.Args.from_parsed_dict(build_info)
 
   args = args_bf.combine(args_cmd)
 
@@ -164,75 +42,17 @@ def main() -> None:
   obj_path = project_path.joinpath(args.obj)
   incl_paths = [project_path.joinpath(i) for i in args.incl]
 
-  if args.init:
-    important(f"Initializing project at {root_path}")
-    root_path.mkdir(parents=True, exist_ok=True)
-    important(f"* {src_path}")
-    src_path.mkdir(parents=True, exist_ok=True)
-    important(f"* {bin_path}")
-    bin_path.mkdir(parents=True, exist_ok=True)
-    important(f"* {obj_path}")
-    obj_path.mkdir(parents=True, exist_ok=True)
-
-    if args.incl != []:
-      for i in args.incl:
-        ip = root_path.joinpath(i)
-        important(f"* {ip}")
-        ip.mkdir(parents=True, exist_ok=True)
-
-    bf_mode = "wt"
-    if not build_file.exists():
-      bf_mode = "xt"
-
-    important(f"* {build_file}")
-    with build_file.open(bf_mode) as f:
-      f.write(f"# automatically generated {datetime.now()}\n")
-      f.write(f"cc: '{args.cc}'\n")
-      f.write(f"exe: '{args.exe}'\n")
-      if src_path != root_path:
-        f.write(f"src: '{src_path.as_posix()}'\n")
-      if bin_path != root_path:
-        f.write(f"bin: '{bin_path.as_posix()}'\n")
-      if obj_path != root_path:
-        f.write(f"obj: '{obj_path.as_posix()}'\n")
-      if args.libs != []:
-        f.write("libs:\n")
-        for l in args.libs:
-          f.write(f"  - '{l}'\n")
-      if args.link != []:
-        f.write("link:\n")
-        for l in args.link:
-          f.write(f"  - '{l}'\n")
-      if args.incl != []:
-        f.write("incl:\n")
-        for i in args.incl:
-          f.write(f"  - '{i.as_posix()}'\n")
-
-    span = time()-start
-    important(f"Finshed in {span:.1f}s")
-    return
-
-  if not project_path.exists():
-    err(f"Provided project directory does not exist: {project_path.absolute()}")
-    return
-  if not root_path.exists():
-    err(f"Provided root directory does not exist: {root_path.absolute()}")
-    return
-  if not src_path.exists():
-    err(f"Provided src directory does not exist: {src_path.absolute()}")
-    return
-
   thejob = job.Job(
     root_path, src_path, bin_path, obj_path,
     args.exe, args.cc, args.libs, args.link, incl_paths,
     args.clean, args.debug,
     False
   )
-  if not thejob.act():
-    return
 
-  span = time()-start
-  important(f"Successfully built in {span:.1f}s")
+  if args.init:
+    cli.init(thejob, build_file)
+  else:
+    cli.build(thejob, project_path)
 
 if __name__ == "__main__":
   main()
---
+++ qcbs/cli.py
@@ -0,0 +1,206 @@
+import qcbs.job as job
+from qcbs.common import *
+from pathlib import Path
+from dataclasses import dataclass
+from typing import Any
+from time import time
+from datetime import datetime
+import subprocess
+import sys
+
+def run_cmd_in(cwd: Path, cmd: str) -> bool:
+  return subprocess.run(cmd,
+    cwd=cwd,
+    shell=True,
+    stdout=sys.stderr,
+    stderr=sys.stderr) == 0
+
+@dataclass
+class Args:
+  project: str
+  root: str
+  src: str
+  bin: str
+  obj: str
+
+  cc: str
+  exe: str
+  libs: list[str]
+  link: list[str]
+  incl: list[Path]
+
+  clean: bool
+  debug: bool
+  init: bool
+
+  @classmethod
+  def parse(cls, args: list[str]) -> "Args":
+    pos = []
+    named: dict[str, Any] = {}
+    for a in args:
+      if a.startswith("--"):
+        if '=' in a[2:]:
+          name, *value_elems = a[2:].split('=')
+          named[name] = '='.join(value_elems)
+        else:
+          named[a[2:]] = True
+      else:
+        pos.append(a)
+
+    project = "."
+    if len(pos) > 1:
+      project = pos[0]
+    if len(pos) > 2:
+      warn("Excess positional arguments ignored.")
+
+    parsed = Args.from_raw_dict(named)
+
+    parsed.clean = named.get("clean", False)
+    parsed.debug = named.get("debug", False)
+    parsed.init = named.get("init", False)
+
+    return parsed
+
+  @classmethod
+  def from_raw_dict(cls, data: dict[str, str]) -> "Args":
+    project = "."
+    root = data.get("root", project)
+    src = data.get("src", root)
+    bin = data.get("bin", root)
+    obj = data.get("obj", bin)
+    exe = data.get("exe", "")
+    cc = data.get("cc", "")
+    libs: list[str] = data.get("lib", "").split(",")
+    link: list[str] = data.get("link", "").split(",")
+    incl: list[str] = data.get("incl", "").split(",")
+
+    if libs == [""]:
+      libs = []
+    if link == [""]:
+      link = []
+    if incl == [""]:
+      incl = []
+
+    return cls(
+      project, root, src, bin, obj,
+      cc, exe, libs, link, [Path(p) for p in incl],
+      False, False, False
+    )
+
+  @classmethod
+  def from_parsed_dict(cls, data: dict[str, Any]) -> "Args":
+    project = "."
+    root = data.get("root", project)
+    src = data.get("src", root)
+    bin = data.get("bin", root)
+    obj = data.get("obj", bin)
+    exe = data.get("exe", "")
+    cc = data.get("cc", "")
+    libs: list[str] = data.get("lib", [])
+    link: list[str] = data.get("link", [])
+    incl: list[Path] = data.get("incl", [])
+
+    return cls(
+      project, root, src, bin, obj,
+      cc, exe, libs, link, incl,
+      False, False, False
+    )
+
+  @classmethod
+  def default(cls) -> "Args":
+    project = "."
+    return cls(
+      project, project, project, project, project,
+      "", "", [], [], [],
+      False, False, False
+    )
+
+  def combine(self, other: "Args") -> "Args":
+    project = other.project if other.project != self.project else self.project
+    root = other.root if other.root != self.root else self.root
+    src = other.src if other.src != self.src else self.src
+    bin = other.bin if other.bin != self.bin else self.bin
+    obj = other.obj if other.obj != self.obj else self.obj
+    exe = other.exe if other.exe != self.exe else self.exe
+    cc = other.cc if other.cc != self.cc else self.cc
+    libs = other.libs if other.libs != self.libs else self.libs
+    link = other.link if other.link != self.link else self.link
+    incl = other.incl if other.incl != self.incl else self.incl
+    return Args(
+      project, root, src, bin, obj,
+      cc, exe, libs, link, incl,
+      self.debug or other.debug, self.clean or other.clean, self.init or other.init
+    )
+
+def init(j: job.Job, build_file: Path) -> None:
+  start = time()
+  important(f"Initializing project at {j.root}")
+  j.root.mkdir(parents=True, exist_ok=True)
+  important(f"* git init")
+  if run_cmd_in(j.root, "git init ."):
+    gitignore = j.root.joinpath(".gitignore")
+    important(f"* {gitignore}")
+    with gitignore.open("xt") as f:
+      f.write(f"# automatically generated {datetime.now()}\n")
+      if j.bin != j.root:
+        f.write(f"{j.bin.as_posix()}\n")
+      if j.obj != j.root:
+        f.write(f"{j.obj.as_posix()}\n")
+  important(f"* {j.src}")
+  j.src.mkdir(parents=True, exist_ok=True)
+  important(f"* {j.bin}")
+  j.bin.mkdir(parents=True, exist_ok=True)
+  important(f"* {j.obj}")
+  j.obj.mkdir(parents=True, exist_ok=True)
+
+  if j.incl != []:
+    for i in j.incl:
+      important(f"* {i}")
+      i.mkdir(parents=True, exist_ok=True)
+
+  bf_mode = "wt"
+  if not build_file.exists():
+    bf_mode = "xt"
+
+  important(f"* {build_file}")
+  with build_file.open(bf_mode) as f:
+    f.write(f"# automatically generated {datetime.now()}\n")
+    f.write(f"cc: '{j.cc}'\n")
+    f.write(f"exe: '{j.exe}'\n")
+    if j.src != j.root:
+      f.write(f"src: '{j.src.as_posix()}'\n")
+    if j.bin != j.root:
+      f.write(f"bin: '{j.bin.as_posix()}'\n")
+    if j.obj != j.root:
+      f.write(f"obj: '{j.obj.as_posix()}'\n")
+    if j.libs != []:
+      f.write("libs:\n")
+      for l in j.libs:
+        f.write(f"  - '{l}'\n")
+    if j.link != []:
+      f.write("link:\n")
+      for l in j.link:
+        f.write(f"  - '{l}'\n")
+    if j.incl != []:
+      f.write("incl:\n")
+      for i in j.incl:
+        f.write(f"  - '{i.as_posix()}'\n")
+
+  span = time()-start
+  important(f"Finshed in {span:.1f}s")
+
+def build(j: job.Job, project_path: Path) -> None:
+  start = time()
+  if not project_path.exists():
+    err(f"Provided project directory does not exist: {project_path.absolute()}")
+    return
+  if not j.root.exists():
+    err(f"Provided root directory does not exist: {j.root.absolute()}")
+    return
+  if not j.src.exists():
+    err(f"Provided src directory does not exist: {j.src.absolute()}")
+    return
+
+  if j.act():
+    span = time()-start
+    important(f"Successfully built in {span:.1f}s")
